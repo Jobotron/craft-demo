7001e2941613ee46ca4c5c73a6024fbe063539db
Jobotron <schuldtze@gmail.com>
2021-11-17 19:02:12 -0800

    ...

--- patient_med_schedule/app.py
+++ patient_med_schedule/app.py
@@ -1,20 +1,23 @@
 import json
 #import boto3
+import uuid
 
 # import requests
-
+_TableName = "PatientTable"
+AttrName = "patientId"
 
 def lambda_handler(event, context):
     # add dynamodb integration
+    newId = uuid.uuid4()
+    
+    resp = put_patient_schedule(str(newId), event["name"], event["schedule"]["daysOfWeek"], event["schedule"]["times"])
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "post",
-            # "location": ip.text.replace("\n", "")
-        }),
+        "body": json.dumps({"patientId":resp}),
     }
 
-#def put_patient_schedule(patientId, name, daysOfWeek, times):
+def put_patient_schedule(patientId, name, daysOfWeek, times):
      # get dynamodb client and perform operation
-
-#    return response
+   #client = boto3.client("dynamodb")
+  # client.put_item(
+   return patientId
