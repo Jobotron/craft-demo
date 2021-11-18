f7391b0c45289a483d4956617b84da2ecc3070e6
Jobotron <schuldtze@gmail.com>
2021-11-17 23:25:01 -0800

    parsed body for post

--- patient_med_schedule/app.py
+++ patient_med_schedule/app.py
@@ -9,15 +9,14 @@ AttrName = "patientId"
 def lambda_handler(event, context):
     # add dynamodb integration
     newId = uuid.uuid4()
+    data = json.loads(event["body"])
     
-    resp = put_patient_schedule(str(newId), event["name"], event["schedule"]["daysOfWeek"], event["schedule"]["times"])
+    resp = put_patient_schedule(str(newId), data["name"], data["schedule"])
     return {
         "statusCode": 200,
-        "body": json.dumps({"patientId":resp}),
+        "body": json.dumps({"patientId": resp}),
     }
 
-def put_patient_schedule(patientId, name, daysOfWeek, times):
-     # get dynamodb client and perform operation
-   #client = boto3.client("dynamodb")
-  # client.put_item(
-   return patientId
+def put_patient_schedule(patientId, name, schedule):
+    print(schedule)
+    return patientId
